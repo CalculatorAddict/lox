8b3a4e37b0fe2713695c657857ecd67e7393ddaf
Arav Bhattacharya <bhattacharya.arav05@gmail.com>
2026-09-05 14:58:54 +0100

    jlox: statements and state

--- jlox/src/com/craftinginterpreters/lox/Lox.java
+++ jlox/src/com/craftinginterpreters/lox/Lox.java
@@ -42,6 +42,7 @@ public class Lox {
       String line = reader.readLine();
       if (line == null) break;
       run(line);
+      hadError = false;
     }
   }
 
@@ -50,12 +51,12 @@ public class Lox {
     List<Token> tokens = scanner.scanTokens();
 
     Parser parser = new Parser(tokens);
-    Expr expression = parser.parse();
+    List<Stmt> statements = parser.parse();
 
     // Stop if there was a syntax error.
     if (hadError) return;
 
-    interpreter.interpret(expression);
+    interpreter.interpret(statements);
   }
 
   static void error(int line, String message) {
